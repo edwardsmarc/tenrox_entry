294d1b117fbb06a1ee0c9bc3c66d6bc5cab921bf
Marc Edwards <marc.edwards@natureconservancy.ca>
2023-02-16 15:16:01 -0800

    initial commit

---
+++ app.R
@@ -0,0 +1,53 @@
+source("global.r")
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Tenrox entry app"),
+  dashboardSidebar(
+    fileInput(inputId = "csv_path", label = "Upload calendar.csv", accept = ".csv")
+  ),
+  dashboardBody(
+    tags$style(HTML(
+      ".reactiveExcelR {
+                    height: calc(100vh - 200px);
+                    }
+                    "
+    )),
+    fluidRow(
+      tabBox(
+        id = "tabs", width = 12,
+        tabPanel("Raw", excelOutput(outputId = "full_table", height = "100%", width = "100%")),
+        tabPanel("Project sums", excelOutput(outputId = "weekly_table", height = "100%", width = "100%"))
+      )
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  # generate reactiveValue to hold data
+  data <- reactiveValues()
+  
+  # load timesheet
+  observe({
+    req(input$csv_path)
+    data$timesheet <- timesheet_prep(read_csv(input$csv_path$datapath))
+    
+    data$full <- full_table(data$timesheet)
+    data$weekly <- weekly_sums(data$full)
+    
+  })
+    
+  # Render tables
+  output$full_table <- renderExcel({
+    req(input$csv_path)
+    excelTable(data$full, autoWidth=TRUE, tableHeight="100%")
+  })
+  
+  output$weekly_table <- renderExcel({
+    req(input$csv_path)
+    excelTable(data$weekly, autoWidth=TRUE, tableHeight="100%")
+  })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ global.R
@@ -0,0 +1,49 @@
+library(shiny)
+library(shinydashboard)
+library(tidyverse)
+library(lubridate)
+library(excelR)
+
+lastmon <- function(x) 7 * floor(as.numeric(x-1+4)/7) + as.Date(1-4, origin="1970-01-01")
+
+
+timesheet_prep <- function(timesheet){
+  
+  timesheet %>% 
+  mutate(duration = (timesheet$`End Time` - timesheet$`Start Time`)/60/60,
+         date = as_date(`Start Date`),
+         mdy = paste(month(date, label = T), day(date), year(date), sep = "/")) %>%
+  filter(!is.na(Categories)) %>%
+  arrange(date, `Start Time`)
+}
+
+
+full_table <- function(prepped_timesheet){
+  
+  tibble(`Date (M/D/Y)` = prepped_timesheet$mdy,
+         Week = as.character(lastmon(prepped_timesheet$date)),
+         Hours = as.numeric(prepped_timesheet$duration),
+         `Project/Activity` = prepped_timesheet$Categories,
+         `Deliverable/Details` = prepped_timesheet$Subject) %>%
+    filter(Week <= lastmon(Sys.Date()))
+}
+
+
+weekly_sums <- function(x){
+  x %>% 
+    group_by(`Project/Activity`, Week) %>% 
+    summarise(Hours = sum(Hours)) %>%
+    arrange(Week)
+}
+
+render_table_function <- function(df, csv_path){
+  
+  renderExcel({
+    
+    req(csv_path)
+    
+    excelTable(df)
+    
+  })
+  
+}
